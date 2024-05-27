53ed10eec3b607d6e12c97a9ad1d179b53e7476a
mahdi <mahdimamashli1383@gmail.com>
2024-05-28 02:37:49 +0330

    "Modified RESP protocol generator to format pattern with IP address and its length."

--- protocol/resp-protocol.py
+++ protocol/resp-protocol.py
@@ -2,7 +2,7 @@ import sys
 def generate_resp(ips):
     pattern = '*3\r\n$3\r\nSET\r\n${0}\r\n{1}\r\n$1\r\n1\r\n'
     for ip in ips:
-        sys.stdout.write(pattern)
+        sys.stdout.write(pattern.format(len(ip),ip))
 data = ''
 with open("ip.source.txt", 'rt') as f:
     data = f.read()
