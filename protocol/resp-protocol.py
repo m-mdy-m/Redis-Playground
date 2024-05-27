30cf4a86fb5d2a09dbd6274d94e008690dd4b1ea
mahdi <mahdimamashli1383@gmail.com>
2024-05-28 02:19:02 +0330

    add python example

---
+++ protocol/resp-protocol.py
@@ -0,0 +1,11 @@
+import sys
+def generate_resp(ips):
+    pattern = '*3\r\n$3\r\nSET\r\n${0}\r\n{1}\r\n$1\r\n1\r\n'
+    for ip in ips:
+        sys.stdout.write(pattern)
+data = ''
+with open("ip.source.txt", 'rt') as f:
+    data = f.read()
+ips = data.split('\n')
+
+generate_resp(ips)
