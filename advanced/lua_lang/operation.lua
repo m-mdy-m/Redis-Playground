f44a0580bea5ef314abcf8dadffb4a18fa0b3d7c
mahdi <mahdimamashli1383@gmail.com>
2024-06-10 14:03:20 +0330

    "Added examples of numbers and strings in Lua, including printing and escaping characters."

--- advanced/lua_lang/operation.lua
+++ advanced/lua_lang/operation.lua
@@ -14,3 +14,25 @@ n3 = 4.57e-3
 n4 = 0.3e12
 n5 = 5e+20
 print(n1,n2,n3,n4,n5)
+
+
+-- Strings
+-- =============================================
+
+-- 1. Lua is eight-bit clean and to strings may contains
+--     characters with any numric value, including embedded zeros.
+
+-- 2. You can store any binary data into a string too.
+-- 3. Automatic memory managment
+
+
+a = "this is a line"
+b = "this is a second line"
+-- print(a,b)
+
+-- \n
+-- \r
+-- \""
+-- \'
+
+print("first line \nsecond line")
