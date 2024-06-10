0b4491ce1a93632d59d271a012d41ce1b1c73821
mahdi <mahdimamashli1383@gmail.com>
2024-06-10 14:35:38 +0330

    add lua tables

---
+++ advanced/lua_lang/lua_tables.lua
@@ -0,0 +1,55 @@
+--[[
+
+Lua - Tables Data Types
+================================================
+1. They are similar to Redis Hashes
+2. Its associated arrays
+3. No fixed size - Add as manay elements you want!
+4. Tables are used to represent;
+    - arrays
+    - symbol tables
+    - sets
+    - records
+    - queues and more.
+5. Tables are objects i.e
+    Keys and Values
+6. Syntax to create a table 
+
+    variable = {}
+
+
+]]
+
+-- 1. Create a table
+
+
+
+-- 2. Add a value 
+
+
+
+-- 3. Assign a key
+
+
+
+-- 4. Create a single dimension table with pre-define values
+
+
+
+-- 5. How to access data elements in a table
+
+
+-- 6. For single dimension table, can we put start index at 0 and not 1?
+
+
+-- 7. Create a key=value pairs tables
+
+
+
+-- 8. Directly assign a value to a table key
+
+
+-- 9. Remove a field
+
+-- 10. tables are long data tables
+
