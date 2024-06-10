971bdf7d2c3ff01ac6fa0e2a8690d131559df5f7
mahdi <mahdimamashli1383@gmail.com>
2024-06-10 15:04:04 +0330

    "Modified lua_statements.lua: commented out print statements, added a loop section with a for loop, and created a table with 50 elements."

--- advanced/lua_lang/lua_statements.lua
+++ advanced/lua_lang/lua_statements.lua
@@ -4,8 +4,26 @@ if x > 51 then
 elseif x==10 then
     print("X = 10")
 else 
-    print("X is not there")
+    -- print("X is not there")
 end
 
 
-y = x > 5 and print("X> 5")
+-- y = x > 5 and print("X> 5")
+
+
+
+----------===========================
+print("LOOP SECTION")
+
+for num1= 1,10
+do
+    -- print(num1)
+end
+
+-- Create 50 new Elm
+t = {}
+for i=1,50
+do
+    t[i]=i+10
+end
+print(t[1],t[2])
