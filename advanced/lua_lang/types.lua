2d8390e6f52808bfadd0839f96b9d02cfef12a69
mahdi <mahdimamashli1383@gmail.com>
2024-06-10 13:45:13 +0330

    add types lua

---
+++ advanced/lua_lang/types.lua
@@ -0,0 +1,25 @@
+-- Values and Data Types
+-- =======================
+
+--[[ 
+    
+1. Lua is a dynamic typed language 
+  - This means that variables do not have types; only values do.
+
+2. Data types 
+    Nil 
+    Boolean
+    Numbers
+    Strings
+    Tables   - Similar to Redis Hashes
+    Functions
+
+--]]
+
+-- 3. Lets print a number and a Nil object
+
+print('Hello')
+a = 2
+b= 3
+
+print(a + b)
